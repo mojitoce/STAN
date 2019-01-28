c7bfcf52eb3c4525a21f4446ad9797b5083f824f
Cesare De Michelis <cesare.demichelis@afiniti.com>
2019-01-28 12:20:19 +0000

    Added previous changes

--- casestudybaseball/binarytrials.R
+++ casestudybaseball/binarytrials.R
@@ -1,3 +1,13 @@
 library(data.table)
+setwd("~/Learning/Maths and Stats/STAN/Case Study - Baseball")
 
-a <- fread(file = 'efronmorris.txt')
+df <- fread(file = 'efronmorris.txt')
+
+batting.stats <- df[, list(FirstName, LastName, Hits, `At-Bats`,
+                           `RemainingAt-Bats`, RemainingHits = SeasonHits - Hits)]
+
+N <- dim(batting.stats)[1]
+K <- batting.stats$`At-B`
+y <- batting.stats$Hits
+K_new <- batting.stats$`RemainingAt-Bats`
+y_new <- batting.stats$RemainingHits
---
+++ casestudybaseball/complete_pooling.stan
@@ -0,0 +1,13 @@
+data {
+  int<lower=0> N; // items
+  int<lower=0> K[N]; // initial trials
+  int<lower=0> // initial sucesses
+
+  int<lower=0> K_new[N]; // new trials
+  int<lower=0> y_new[N]' // new successes
+
+  }
+
+parameters {
+
+}
